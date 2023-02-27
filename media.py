c5e9d8819452310d69c51a4f55d9015ebe913289
Luiz Felipe Santos <111072221+LordLuizFelipe@users.noreply.github.com>
2023-02-27 20:24:53 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,12 @@
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+nota3 = float(input("Informe a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media == 10:
+    print("Aprovado com Distinção. Média:", media)
+elif media >= 7:
+    print("Aprovado. Média:", media)
+else:
+    print("Reprovado. Média:", media)
